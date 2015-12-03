0388824e3f7fcdeb540f6652c39c7866542f8a36
RAK <rkilcoyne@cloudbolt.io>
2015-12-03 15:25:21 -0500

    Added **kwargs to handle service calling convention

--- actions/cloudbolt_plugins/generate_options/generate_expiration_date_options/generate_expiration_date_options.py
+++ actions/cloudbolt_plugins/generate_options/generate_expiration_date_options/generate_expiration_date_options.py
@@ -6,7 +6,7 @@ the expiration date parameter based on offsets from the current day
 """
 
 
-def get_options_list(field, environment=None, group=None):
+def get_options_list(field, environment=None, group=None, **kwargs):
     six_months = datetime.datetime.now() + datetime.timedelta(days=180)
     one_month = datetime.datetime.now() + datetime.timedelta(days=30)
     one_week = datetime.datetime.now() + datetime.timedelta(days=7)
