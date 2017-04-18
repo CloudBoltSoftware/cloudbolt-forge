f3ad162602d0a071571bee3c3ac85c300ea4be33
ack81 <akinney@cloudboltsoftware.com>
2017-04-18 07:16:45 -0700

    Fix get_session args for tintri actions

--- ui_extensions/tintri/tintri_action_clone/tintri_action_clone/cb_plugin_1482264711802934_AHbNgX7.py
+++ ui_extensions/tintri/tintri_action_clone/tintri_action_clone/cb_plugin_1482264711802934_AHbNgX7.py
@@ -3,7 +3,7 @@ from xui.tintri import views as t
 
 def run(job, logger=None, **kwargs):
     server = job.server_set.first()
-    tintri = t.get_session()
+    tintri = t.get_session(server)
     tintri_vm = t.get_vm(tintri, server.hostname)
     t.vm_clone(tintri,
                tintri_vm,
--- ui_extensions/tintri/tintri_action_snapshot/tintri_action_snapshot/cb_plugin_1482264675724517_zr86kMC.py
+++ ui_extensions/tintri/tintri_action_snapshot/tintri_action_snapshot/cb_plugin_1482264675724517_zr86kMC.py
@@ -3,7 +3,7 @@ from xui.tintri import views as t
 
 def run(job, logger=None, **kwargs):
     server = job.server_set.first()
-    tintri = t.get_session()
+    tintri = t.get_session(server)
     vm = t.get_vm(tintri, server.hostname)
     # Snapshot retention default is 4 hours
     t.vm_snapshot(tintri,
