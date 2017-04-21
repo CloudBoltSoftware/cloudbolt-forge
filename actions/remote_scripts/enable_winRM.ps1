911961370affa1aaf085ec6742cc36297d06e2ae
Aaron Jab <aaronjab@gmail.com>
2017-04-21 10:17:21 -0400

    Update enable_winRM.ps1

--- actions/remote_scripts/enable_winRM.ps1
+++ actions/remote_scripts/enable_winRM.ps1
@@ -3,3 +3,4 @@ winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 Start-Service WinRM
 set-service WinRM -StartupType Automatic
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled false
