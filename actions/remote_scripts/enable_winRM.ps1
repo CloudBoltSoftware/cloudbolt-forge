8e51a60507321db9fc4bb63fff2aaf663f60a780
Aaron Jab <aaronjab@gmail.com>
2017-04-21 10:13:25 -0400

    Update enable_winRM.ps1

--- actions/remote_scripts/enable_winRM.ps1
+++ actions/remote_scripts/enable_winRM.ps1
@@ -1,4 +1,5 @@
+winrm quickconfig -q
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/service/client '@{Basic="true"}'
-Restart-Service winrm
+Start-Service WinRM
+set-service WinRM -StartupType Automatic
