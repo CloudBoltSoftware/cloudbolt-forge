120f907695d041dbdf8613d6d6932d0fb691cb0e
Thomas Hamlin <thomas@cloudboltsoftware.com>
2017-04-26 17:06:35 -0700

    Add docstrings to Tintri action scripts

--- ui_extensions/tintri/tintri_action_clone/tintri_action_clone/cb_plugin_1482264711802934_AHbNgX7.py
+++ ui_extensions/tintri/tintri_action_clone/tintri_action_clone/cb_plugin_1482264711802934_AHbNgX7.py
@@ -1,3 +1,15 @@
+"""
+This plugin is required as part of the Tintri UI Extension. It provides one of
+the actions exposed on a Tintri server tab.
+
+Prompts user for one runtime input: name of new VM.
+
+This Server Action should be Disabled so that it isn't shown as a button on server
+detail pages. Instead, it's rendered directly by the Tintri UI extension.
+
+More about Tintri clones:
+https://www.tintri.com/blog/2012/04/accelerating-virtualization-advanced-vm-snapshots-and-clones
+"""
 from xui.tintri import views as t
 
 
--- ui_extensions/tintri/tintri_action_snapshot/tintri_action_snapshot/cb_plugin_1482264675724517_zr86kMC.py
+++ ui_extensions/tintri/tintri_action_snapshot/tintri_action_snapshot/cb_plugin_1482264675724517_zr86kMC.py
@@ -1,3 +1,15 @@
+"""
+This plugin is required as part of the Tintri UI Extension. It provides one of
+the actions exposed on a Tintri server tab.
+
+This Server Action should be Disabled so that it isn't shown as a button on server
+detail pages. Instead, it's rendered directly by the Tintri UI extension.
+
+Prompts user for one runtime input: snapshot name.
+
+More about Tintri snapshots:
+https://www.tintri.com/blog/2012/04/accelerating-virtualization-advanced-vm-snapshots-and-clones
+"""
 from xui.tintri import views as t
 
 
