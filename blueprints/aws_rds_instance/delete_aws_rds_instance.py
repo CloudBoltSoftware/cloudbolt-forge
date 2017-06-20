2aae7b1718bfc21267922f7fe09dcf47be69582b
Thomas Hamlin <thomas@cloudboltsoftware.com>
2017-06-20 14:56:18 -0700

    Use code consistent with other RDS actions
    [#144244831]

--- blueprints/aws_rds_instance/delete_aws_rds_instance.py
+++ blueprints/aws_rds_instance/delete_aws_rds_instance.py
@@ -6,22 +6,19 @@ from infrastructure.models import Environment
 
 def run(job, logger=None, **kwargs):
     service = job.service_set.first()
-    env_id = service.attributes.get(field__name__startswith='aws_environment').value
-    env = Environment.objects.get(id=env_id)
-    rh = env.resource_handler.cast()
 
-    job.set_progress('Connecting to AWS...')
-    client = boto3.client(
-        'rds',
-        region_name=env.aws_region,
-        aws_access_key_id=rh.serviceaccount,
-        aws_secret_access_key=rh.servicepasswd)
-
+    # The Environment ID and RDS Instance data dict were stored as attributes on
+    # this service by a build action.
+    env_id_cfv = service.attributes.get(field__name__startswith='aws_environment')
     instance_cfv = service.attributes.get(field__name='rds_instance')
+
+    env = Environment.objects.get(id=env_id_cfv.value)
+    client = connect_to_rds(env)
+
     instance = json.loads(instance_cfv.value)
     identifier = instance['identifier']
 
-    job.set_progress('Deleting RDS instance {}...'.format(identifier))
+    job.set_progress('Deleting RDS instance {0}...'.format(identifier))
     response = client.delete_db_instance(
         DBInstanceIdentifier=identifier,
         # AWS strongly recommends taking a final snapshot before deleting a DB.
@@ -33,3 +30,16 @@ def run(job, logger=None, **kwargs):
 
     job.set_progress('RDS instance {0} deleted.'.format(identifier))
     return 'SUCCESS', '', ''
+
+
+def connect_to_rds(env):
+    """
+    Return boto connection to the RDS in the specified environment's region.
+    """
+    job.set_progress('Connecting to AWS RDS in region {0}.'.format(env.aws_region))
+    rh = env.resource_handler.cast()
+    return boto3.client(
+        'rds',
+        region_name=env.aws_region,
+        aws_access_key_id=rh.serviceaccount,
+        aws_secret_access_key=rh.servicepasswd)
