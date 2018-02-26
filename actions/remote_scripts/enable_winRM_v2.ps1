de048ad6956a21aeedaf5ccb489f2ccedb4d8756
R.A. Kilcoyne <rkilcoyne@gmail.com>
2018-02-25 23:24:54 -0500

    Ensure we can get a reference to the TS setting.

--- actions/remote_scripts/enable_winRM_v2.ps1
+++ actions/remote_scripts/enable_winRM_v2.ps1
@@ -4,4 +4,7 @@ winrm set winrm/config/service/auth '@{Basic="true"}'
 Start-Service WinRM
 set-service WinRM -StartupType Automatic
 
-(Get-WmiObject -class Win32_TSGeneralSetting -Namespace root\cimv2\terminalservices).SetUserAuthenticationRequired(0)
+$result = (Get-WmiObject -class Win32_TSGeneralSetting -Namespace root\cimv2\terminalservices)
+if ($result) {
+  $result.SetUserAuthenticationRequired(0)
+}
