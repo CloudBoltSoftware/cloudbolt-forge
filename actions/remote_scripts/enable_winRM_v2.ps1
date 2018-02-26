c6d73f50a5e631f5a0a1455fd1f3420c487dc81e
R.A. Kilcoyne <rkilcoyne@gmail.com>
2018-02-25 23:13:21 -0500

    Disable NLA for RDP access

--- actions/remote_scripts/enable_winRM_v2.ps1
+++ actions/remote_scripts/enable_winRM_v2.ps1
@@ -3,3 +3,5 @@ winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 Start-Service WinRM
 set-service WinRM -StartupType Automatic
+
+(Get-WmiObject -class Win32_TSGeneralSetting -Namespace root\cimv2\terminalservices).SetUserAuthenticationRequired(0)
