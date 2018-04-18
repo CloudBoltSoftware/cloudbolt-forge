0f84e56a54ae993f6002e71cb52c3a10d31525eb
Maryam Faiz <maryamfafaiz@gmail.com>
2018-04-18 19:11:06 -0400

    Added randomly change CloudBolt colors plugin

---
+++ actions/cloudbolt_plugins/cb_plugin_1522594340048638_Mi99bP9.py
@@ -0,0 +1,46 @@
+"""
+This is a working sample CloudBolt plug-in for you to change your portals color randomly 
+"""
+from random import randint
+
+from common.methods import set_progress
+from portals.models import PortalConfig
+from utilities.colors import rgb_to_hex
+
+def run(*args, **kwargs):
+    only_change_default_portal = "{{ only_change_default_portal }}" == "True"
+
+    if only_change_default_portal:
+        set_progress("Changing colors only for the default portal")
+        # get_current_portal() is used here not to get the current user's portal (since this is
+        # run in the context of an asynchronous job, there is no current portal), but instead to
+        # get the default portal.
+        portals = [PortalConfig.get_current_portal()]
+    else:
+        set_progress("Changing colors only for all portals")
+        portals = PortalConfig.objects.all()
+
+    for portal in portals:
+        set_random_colors(portal)
+    
+    return "SUCCESS", "Portal colors changed", ""
+
+
+def set_random_colors(portal):
+    fields = [
+        'banner_bg_color',
+        'topnav_bg_color',                                                  
+        'topnav_text_color',
+        'content_bg_color',
+        'content_text_color',                                               
+        'heading_text_color',                                               
+        'tooltip_bg_color',                                                 
+        'tooltip_text_color',
+        'security_bg_color',
+        'security_text_color',
+    ]
+    
+    for f in fields:
+        setattr(portal, f, rgb_to_hex([randint(0,255), randint(0,255), randint(0,255)]))
+        set_progress("Changing {} to {}".format(f, getattr(portal, f)))
+    portal.save()
