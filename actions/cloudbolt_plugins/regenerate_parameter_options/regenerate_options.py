37927017b643a602ffd1155443a0f4039cdeac0f
kofran88 <guy.francis.kono8@gmail.com>
2018-07-07 10:36:27 -0400

    Update regenerate_options.py

--- actions/cloudbolt_plugins/regenerate_parameter_options/regenerate_options.py
+++ actions/cloudbolt_plugins/regenerate_parameter_options/regenerate_options.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from common.methods import set_progress
+
 """
 An example for creating plug-ins that regenerate field options for a dependent
 parameter based on choices made in previous parameter fields.
@@ -19,14 +21,14 @@ To make a parameter dependent on another:
 
 
 """Create Action Inputs for desired form fields"""
-Monitoring_Tool = {{ monitoring_tool }}
-Mode = {{ mode }}
+monitoring_tool = "{{ monitoring_tool }}"
+mode = "{{ mode }}"
 
 
 def generate_options_for_monitoring_tool(server=None, **kwargs):
     """
     Define a list of tuples that will be returned to generate the field options
-    for the Monitoring_Tool Action Input.
+    for the monitoring_tool Action Input.
 
     In this example Mode is dependent on Monitoring_Tool. Dependencies between
     parameters can be defined within the dependent parameter's page.
@@ -45,7 +47,7 @@ def generate_options_for_monitoring_tool(server=None, **kwargs):
 def generate_options_for_mode(server=None, control_value=None, **kwargs):
     """
     Define a list of tuples that will be returned to generate the field options
-    for the Mode Action Input, depending on the value from Monitoring_Tool
+    for the mode Action Input, depending on the value from monitoring_tool
     passed in as the control_value argument.
 
     each tuple follows this order: (value, label)
@@ -60,3 +62,9 @@ def generate_options_for_mode(server=None, control_value=None, **kwargs):
                 ('service', 'Service'), ('cloud', 'Cloud'),
                 ('databases', 'Databases'), ('storage', 'Storage')
                 ]
+
+def run(job, **kwargs):
+    """Display Action Inpupt values."""
+    # Incomplete
+    set_progress()
+    return "", "", ""
