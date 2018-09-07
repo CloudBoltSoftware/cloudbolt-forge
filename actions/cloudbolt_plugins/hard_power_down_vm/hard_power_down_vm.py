a1b21859f8f09e575254fc44aea8cca6f57376cf
WebSpider <webspider@users.noreply.github.com>
2018-09-07 12:35:51 +0200

    Change plugin to run better when selecting multiple servers

--- actions/cloudbolt_plugins/hard_power_down_vm/hard_power_down_vm.py
+++ actions/cloudbolt_plugins/hard_power_down_vm/hard_power_down_vm.py
@@ -1,12 +1,10 @@
-import sys
-import time
 import pyVmomi
 
 from common.methods import set_progress
-from infrastructure.models import Server
 from resourcehandlers.vmware.pyvmomi_wrapper import get_vm_by_uuid, wait_for_tasks
 from resourcehandlers.vmware.models import VsphereResourceHandler
 from resourcehandlers.vmware.vmware_41 import TechnologyWrapper
+from utilities.logger import ThreadLogger
 
 
 def get_vmware_service_instance(rh):
@@ -28,10 +26,17 @@ def get_vmware_service_instance(rh):
 
 def run(job, logger=None, server=None, **kwargs):
 
+    if not logger:
+        logger = ThreadLogger(__name__)
+
     si = None
     for server in job.server_set.all():
+        if server.resource_handler.type_slug != 'vmware':
+            logger.info("Server {} is not on VMWare, skipping".format(server.hostname))
+            continue
+
         if not si:
-            si = get_vmware_service_instance(server.resource_handler.cast())
+            si = get_vmware_service_instance(server.resource_handler)
         vm = get_vm_by_uuid(si, server.resource_handler_svr_id)
 
         assert isinstance(vm, pyVmomi.vim.VirtualMachine)
@@ -39,8 +44,15 @@ def run(job, logger=None, server=None, **kwargs):
         server.refresh_info()
 
         server_original_power_status = server.power_status
-        set_progress("Performing VM power down...")
+
+        if server_original_power_status != "POWERON":
+            logger.info("Server {} is not powered on. Skipping this server".format(server.hostname))
+            continue
+
+        set_progress("Performing VM power down for VM {}".format(server.hostname))
         task = vm.PowerOffVM_Task()
         wait_for_tasks(si, [task])
+
+        server.refresh_info()
 
     return "", "", ""
