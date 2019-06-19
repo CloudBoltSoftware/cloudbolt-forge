1a2e892539cde260934ceffe58d399c5a3222d0c
Ryan Opel <ropel@cloudboltsoftware.com>
2019-06-19 15:41:44 -0700

    Remove logger and fix typos
    [DEV-12140]

--- actions/cloudbolt_plugins/multi_user_approval/two_user_approval.py
+++ actions/cloudbolt_plugins/multi_user_approval/two_user_approval.py
@@ -2,17 +2,16 @@
 Two User Approval
 
 Overrides CloudBolt's standard Order Approval workflow. This Orchestration
-Action requires two users to approve an order before it is becomes Active.
+Action requires two users to approve an order before it becomes Active.
+
+Requires CloudBolt 8.8
 """
-
-from utilities.logger import ThreadLogger
-
-logger = ThreadLogger(__name__)
 
 
 def run(order, *args, **kwargs):
     # Return the order's status to "PENDING" if fewer than two users have
     # approved it.
+
     if len(order.approvers) < 2:
         order.status = "PENDING"
         order.save()
