5b5d6f7ea5e349c1ebb16bed7030273b569726fd
Ryan Opel <ropel@cloudboltsoftware.com>
2019-06-24 11:32:16 -0700

    Update docstrings, group names to match test case
    [DEV-12140]

--- actions/cloudbolt_plugins/multi_user_approval/hierarchical_multi_group_approval_pre_order_execution.py
+++ actions/cloudbolt_plugins/multi_user_approval/hierarchical_multi_group_approval_pre_order_execution.py
@@ -1,21 +1,20 @@
 """
-Hierarchical, Multiple Group Approval: Pre-Order Execution
+Hierarchical, Multiple Group Approval: Post-Order Approval
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Overrides CloudBolt's standard Order Approval workflow. This Orchestration
-Action requires that users from three separate Groups approve an Order in a
+Action requires that users from two separate Groups approve an Order in a
 specified order before it becomes Active.
 
 
 Configuration
 ~~~~~~~~~~~~~
-If the user that submitted this Order belongs to Group_A, this plugin requires
-one of two scenarios:
-    1) Group B, Group C, and Group D all have "Approval Permission" for
-       Group A;
-    2) The approving users are Approvers in all four Groups (i.e. Groups A, B,
-       C, and D).
+If the user that submitted this Order belongs to "Workers", this plugin
+requires one of two scenarios:
+    1) "Finance" and "IT" groups both have "Approval Permission" for "Workers";
+    2) The approving users are Approvers in both Groups (i.e. "Finance" and
+       "IT").
 
-When adding or editing this plugin, Group A must be added to the "Groups"
+When adding or editing this plugin, "Workers" must be added to the "Groups"
 field.
 
 
@@ -30,37 +29,26 @@ from accounts.models import Group
 def run(order, *args, **kwargs):
     # This hook executes after an Order has been approved by a user.
 
-    group_b = Group.objects.get(name="Group_B")
-    group_c = Group.objects.get(name="Group_C")
-    group_d = Group.objects.get(name="Group_D")
+    finance_group = Group.objects.get(name="Finance")
+    it_group = Group.objects.get(name="IT")
 
-    # If at least one user from Group_B has approved this order, assign the
-    # order to "Group C" for approval.
+    # If at least one user from "Finance" has approved this order, assign the
+    # order to "IT" for approval.
     if (
         len(order.approvers) >= 1
-        and order.all_groups_approved([group_b])
+        and order.all_groups_approved([finance_group])
     ):
-        order.groups_for_approval = group_c
+        order.groups_for_approval = it_group
     # The order was approved, but the above conditions were not met. Return the
     # order's status to "PENDING", allowing other users to approve the order.
     else:
         order.set_pending()
 
-    # If at least two users have approved this order, represting both Groups B
-    # and C, assign the order to "Group D" for approval.
+    # If at least two users have approved this order, represting both "Finance"
+    # and "IT" Groups, allow the order to be approved.
     if (
         len(order.approvers) >= 2
-        and order.all_groups_approved([group_b, group_c])
-    ):
-        order.groups_for_approval = group_d
-    else:
-        order.set_pending()
-
-    # If at least three users have approved this order, represting Groups B, C,
-    # and D, allow the order to be approved.
-    if (
-        len(order.approvers) >= 3
-        and order.all_groups_approved([group_b, group_c, group_d])
+        and order.all_groups_approved([finance_group, it_group])
     ):
         pass
     else:
