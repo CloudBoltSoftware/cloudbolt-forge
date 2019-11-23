57906354aecbe5f9b66ec39987f83d8219db7941
Martin Baker <digitalorigins@gmail.com>
2019-11-22 17:29:49 -0800

    add exception handling for ObjectDoesNotExist

--- blueprints/multi_node_kubernetes_cluster/teardown_kubernetes_cluster/teardown_kubernetes_cluster.py
+++ blueprints/multi_node_kubernetes_cluster/teardown_kubernetes_cluster/teardown_kubernetes_cluster.py
@@ -2,6 +2,7 @@
 Teardown the CloudBolt resources (container_orchestrator, environment)
 associated with this Kubernetes cluster.
 """
+from django.core.exceptions import ObjectDoesNotExist
 from common.methods import set_progress
 from containerorchestrators.kuberneteshandler.models import Kubernetes
 from utilities.run_command import execute_command
@@ -10,11 +11,14 @@ from utilities.run_command import execute_command
 def run(job, *args, **kwargs):
     resource = job.resource_set.first()
     
-    container_orchestrator = Kubernetes.objects.get(id=resource.container_orchestrator_id)
-    environment = container_orchestrator.environment_set.first()
+    try:
+        container_orchestrator = Kubernetes.objects.get(id=resource.container_orchestrator_id)
+        environment = container_orchestrator.environment_set.first()
     
-    container_orchestrator.delete()
-    environment.delete()
+        container_orchestrator.delete()
+        environment.delete()
+    except ObjectDoesNotExist:
+        set_progress("Could not find container orchestrator for this resource.")
 
-    resource_dir = '/var/opt/cloudbolt/kubernetes/resource-{}'.format(resource_id)
-    execute_command('rm -rf {}'.format(RESOURCE_LOCATION))
+    resource_dir = '/var/opt/cloudbolt/kubernetes/resource-{}'.format(resource.id)
+    execute_command('rm -rf {}'.format(resource_dir))
