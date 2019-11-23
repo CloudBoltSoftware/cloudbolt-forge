56ac100c8ca357a5600db7a16859cca1483ccb13
zachcalvert <zach@cloudbolt.io>
2019-11-22 16:43:20 -0800

    Remove config files from filesystem on teardown
    [DEV-13843]

--- blueprints/multi_node_kubernetes_cluster/teardown_kubernetes_cluster/teardown_kubernetes_cluster.py
+++ blueprints/multi_node_kubernetes_cluster/teardown_kubernetes_cluster/teardown_kubernetes_cluster.py
@@ -4,6 +4,7 @@ associated with this Kubernetes cluster.
 """
 from common.methods import set_progress
 from containerorchestrators.kuberneteshandler.models import Kubernetes
+from utilities.run_command import execute_command
 
 
 def run(job, *args, **kwargs):
@@ -14,3 +15,6 @@ def run(job, *args, **kwargs):
     
     container_orchestrator.delete()
     environment.delete()
+
+    resource_dir = '/var/opt/cloudbolt/kubernetes/resource-{}'.format(resource_id)
+    execute_command('rm -rf {}'.format(RESOURCE_LOCATION))
