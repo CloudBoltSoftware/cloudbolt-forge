5f045f2b295ead1490f69a856fcd72f5a13b2cb8
Maryam Faiz <maryamfafaiz@gmail.com>
2020-02-10 07:41:28 -0800

    Improved progress message.
    [https://cloudbolt.atlassian.net/browse/DEV-12629]

--- actions/cloudbolt_plugins/recurring_jobs/remove_.zip_from_tmp.py
+++ actions/cloudbolt_plugins/recurring_jobs/remove_.zip_from_tmp.py
@@ -9,7 +9,7 @@ import time
 def run(job, *args, **kwargs):
     
     zip_file_list = glob.glob("/tmp/systemd-private*/tmp/*.zip")
-    set_progress("Found following zip files in /tmp: {}".format(zip_file_list))
+    set_progress("Found following zip files in /tmp/systemd/*/tmp: {}".format(zip_file_list))
     for file in zip_file_list:
         if os.path.getmtime(file) < time.time() - 5 * 60:
 	   set_progress("Removing these files {}".format(file))
