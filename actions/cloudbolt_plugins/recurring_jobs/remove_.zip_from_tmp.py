dccf37aa273eeddaf51c1813d3abae51e9b4bee4
Maryam Faiz <maryamfafaiz@gmail.com>
2020-02-10 10:17:54 -0800

    Addressed PR feedback
    [https://cloudbolt.atlassian.net/browse/DEV-12629]

--- actions/cloudbolt_plugins/recurring_jobs/remove_.zip_from_tmp.py
+++ actions/cloudbolt_plugins/recurring_jobs/remove_.zip_from_tmp.py
@@ -7,10 +7,10 @@ import os
 import time
 
 def run(job, *args, **kwargs):
-    
     zip_file_list = glob.glob("/tmp/systemd-private*/tmp/*.zip")
     set_progress("Found following zip files in /tmp/systemd/*/tmp: {}".format(zip_file_list))
     for file in zip_file_list:
+        #Only remove zip files that are at least 5 minutes old.
         if os.path.getmtime(file) < time.time() - 5 * 60:
 	   set_progress("Removing these files {}".format(file))
            os.remove(file)
