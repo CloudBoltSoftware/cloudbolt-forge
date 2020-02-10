03d4c298add892e603e48ca35b1a5070f407d6ff
Maryam Faiz <maryamfafaiz@gmail.com>
2020-02-10 07:37:03 -0800

    Remove only those files that were stored 5 minutes ago.
    [https://cloudbolt.atlassian.net/browse/DEV-12629]

--- actions/cloudbolt_plugins/recurring_jobs/remove_.zip_from_tmp.py
+++ actions/cloudbolt_plugins/recurring_jobs/remove_.zip_from_tmp.py
@@ -4,13 +4,14 @@ This action removes all the zip files from CloudBolt /tmp/systemd-private* direc
 from common.methods import set_progress
 import glob
 import os
+import time
 
 def run(job, *args, **kwargs):
     
     zip_file_list = glob.glob("/tmp/systemd-private*/tmp/*.zip")
     set_progress("Found following zip files in /tmp: {}".format(zip_file_list))
     for file in zip_file_list:
-        set_progress("Removing these files {}".format(file))
-        os.remove(file)
-
+        if os.path.getmtime(file) < time.time() - 5 * 60:
+	   set_progress("Removing these files {}".format(file))
+           os.remove(file)
     return "SUCCESS", "", ""
