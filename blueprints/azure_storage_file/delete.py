083505569b1f3f238092e01132fe3c428af08293
Daniel Menendez <demenendez@cloudbolt.io>
2020-03-31 12:50:30 -0400

    [DEV-15186]/[DEV-15116] added azure BPs and their cit test to the forge

---
+++ blueprints/azure_storage_file/delete.py
@@ -0,0 +1,27 @@
+"""
+Deletes an Azure storage file
+"""
+from common.methods import set_progress
+from azure.common.credentials import ServicePrincipalCredentials
+from resourcehandlers.azure_arm.models import AzureARMHandler
+from azure.storage.file import FileService
+
+def run(job, **kwargs):
+    resource = kwargs.pop('resources').first()
+
+    file_name = resource.attributes.get(field__name='azure_storage_file_name').value
+    share_name = resource.attributes.get(field__name='azure_storage_file_share_name').value
+    azure_storage_account_name = resource.attributes.get(field__name='azure_storage_account_name').value
+    azure_account_key = resource.attributes.get(field__name='azure_account_key').value
+
+    set_progress("Connecting To Azure...")
+    file_service = FileService(account_name=azure_storage_account_name, account_key=azure_account_key)
+
+    set_progress("Connection to Azure established")
+
+
+    set_progress("Deleting file %s..." % file_name)
+    status = file_service.delete_file(file_name=file_name, share_name=share_name, directory_name='')
+    print("status: {}".format(status))
+
+    return "Success", "The file has been deleted", ""
