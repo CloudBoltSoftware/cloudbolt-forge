12301a6777a30e815a5b5f625da54a5165c85245
Pete Gautier <pgautier@cloudboltsoftware.com>
2020-06-11 14:21:11 -0700

    Adding Cisco UCS blueprint
    [DEV-15466]

---
+++ blueprints/cisco/delete.py
@@ -0,0 +1,27 @@
+from utilities.models import ConnectionInfo
+from ucscsdk.ucschandle import UcscHandle
+from ucsmsdk.ucshandle import UcsHandle
+
+
+conn, _ = ConnectionInfo.objects.get_or_create(name='Cisco UCS')
+# While using the platform emulator for testing, using namedtuple will prevent you from modifying the existing ConnectionInfo since the IP address keeps on changing.
+# from collections import namedtuple
+# conn = namedtuple('ConnectionInfo', ['ip', 'username', 'password', 'port'])
+# conn = conn('192.168.241.159', 'admin', 'admin', '80')
+
+try:
+    handle = UcscHandle(conn.ip, conn.username, conn.password, conn.port)
+except Exception:
+    # not a UCS central server. Connect using UcsHandle
+    handle = UcsHandle(conn.ip, conn.username, conn.password, conn.port)
+
+
+def run(job, resource, *args, **kwargs):
+    # Delete the Service profile
+    if not handle.cookie:
+        handle.login()
+    sp = handle.query_dn(f"{resource.service_profile_server_dn}")
+    handle.remove_mo(sp)
+    handle.commit()
+
+    return "SUCCESS", "", ""
