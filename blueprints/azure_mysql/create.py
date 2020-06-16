d7bab97190f2c21d6063ccc84d475f19fed9a9a5
Nick Beaird <nicklasbeaird@gmail.com>
2020-06-16 10:31:36 -0700

    Add Azure mysql bp in the content library
    [DEV-15784]

---
+++ blueprints/azure_mysql/create.py
@@ -0,0 +1,197 @@
+"""
+Creates an MySql in Azure.
+"""
+import settings
+
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.rdbms import mysql
+from msrestazure.azure_exceptions import CloudError
+
+from common.methods import is_version_newer, set_progress
+from common.mixins import get_global_id_chars
+from infrastructure.models import CustomField, Environment
+
+
+cb_version = settings.VERSION_INFO["VERSION"]
+CB_VERSION_93_PLUS = is_version_newer(cb_version, "9.2.2")
+
+
+def _get_client(handler):
+    """
+    Get the client using newer methods from the CloudBolt main repo if this CB is running
+    a version greater than 9.2. These internal methods implicitly take care of much of the other
+    features in CloudBolt such as proxy and ssl verification.
+    Otherwise, manually instantiate clients without support for those other CloudBolt settings.
+    """
+    set_progress("Connecting to Azure...")
+
+    if CB_VERSION_93_PLUS:
+        from resourcehandlers.azure_arm.azure_wrapper import configure_arm_client
+
+        wrapper = handler.get_api_wrapper()
+        mysql_client = configure_arm_client(wrapper, mysql.MySQLManagementClient)
+    else:
+        # TODO: Remove once versions <= 9.2 are no longer supported.
+        credentials = ServicePrincipalCredentials(
+            client_id=handler.client_id, secret=handler.secret, tenant=handler.tenant_id
+        )
+        mysql_client = mysql.MySQLManagementClient(credentials, handler.serviceaccount)
+
+    set_progress("Connection to Azure established")
+
+    return mysql_client
+
+
+def generate_options_for_env_id(server=None, **kwargs):
+    envs = Environment.objects.filter(
+        resource_handler__resource_technology__name="Azure"
+    )
+    options = [(env.id, env.name) for env in envs]
+    return options
+
+
+def generate_options_for_resource_group(control_value=None, **kwargs):
+    """Dynamically generate options for resource group form field based on the user's selection for Environment.
+    
+    This method requires the user to set the resource_group parameter as dependent on environment.
+    """
+    if control_value is None:
+        return []
+
+    env = Environment.objects.get(id=control_value)
+
+    if CB_VERSION_93_PLUS:
+        # Get the Resource Groups as defined on the Environment. The Resource Group is a
+        # CustomField that is only updated on the Env when the user syncs this field on the
+        # Environment specific parameters.
+        resource_groups = env.custom_field_options.filter(
+            field__name="resource_group_arm"
+        )
+        return [rg.str_value for rg in resource_groups]
+    else:
+        rh = env.resource_handler.cast()
+        groups = rh.armresourcegroup_set.all()
+        return [g.name for g in groups]
+
+
+def create_custom_fields_as_needed():
+    CustomField.objects.get_or_create(
+        name="azure_rh_id",
+        type="STR",
+        defaults={
+            "label": "Azure RH ID",
+            "description": "Used by the Azure blueprints",
+            "show_as_attribute": True,
+        },
+    )
+
+    CustomField.objects.get_or_create(
+        name="azure_database_name",
+        type="STR",
+        defaults={
+            "label": "Azure Database Name",
+            "description": "Used by the Azure blueprints",
+            "show_as_attribute": True,
+        },
+    )
+
+    CustomField.objects.get_or_create(
+        name="azure_server_name",
+        type="STR",
+        defaults={
+            "label": "Azure Server Name",
+            "description": "Used by the Azure blueprints",
+            "show_as_attribute": True,
+        },
+    )
+
+    CustomField.objects.get_or_create(
+        name="azure_location",
+        type="STR",
+        defaults={
+            "label": "Azure Location",
+            "description": "Used by the Azure blueprints",
+            "show_as_attribute": True,
+        },
+    )
+
+    CustomField.objects.get_or_create(
+        name="resource_group_name",
+        type="STR",
+        defaults={
+            "label": "Azure Resource Group",
+            "description": "Used by the Azure blueprints",
+            "show_as_attribute": True,
+        },
+    )
+
+
+def run(job, **kwargs):
+    resource = kwargs.get("resource")
+    create_custom_fields_as_needed()
+    env_id = "{{ env_id }}"
+    env = Environment.objects.get(id=env_id)
+    rh = env.resource_handler.cast()
+    location = env.node_location
+    set_progress("Location: %s" % location)
+
+    resource_group = "{{ resource_group }}"
+
+    database_name = "{{ database_name }}"
+
+    server_name = f"{database_name}-mysql-{get_global_id_chars()}"
+
+    server_username = "{{ server_username }}"
+    server_password = "{{ server_password }}"
+
+    resource.name = "Azure MySql - " + database_name
+    resource.azure_server_name = server_name
+    resource.azure_database_name = database_name
+    resource.resource_group_name = resource_group
+    resource.azure_location = location
+    resource.azure_rh_id = rh.id
+    resource.save()
+
+    client = _get_client(rh)
+
+    set_progress('Checking if server "%s" already exists...' % server_name)
+    try:
+        server = client.servers.get(resource_group, server_name)
+    except CloudError as e:
+        set_progress("Azure Clouderror: {}".format(e))
+    else:
+        # No ResourceNotFound exception; server already exists
+        return (
+            "FAILURE",
+            "Database server already exists",
+            "DB server instance %s exists already" % server_name,
+        )
+
+    set_progress('Creating server "%s"...' % server_name)
+    params = {
+        "location": location,
+        "version": "12.0",
+        "administrator_login": server_username,
+        "administrator_login_password": server_password,
+        "properties": {
+            "create_mode": "Default",
+            "administrator_login": server_username,
+            "administrator_login_password": server_password,
+        },
+    }
+    async_server_create = client.servers.create(resource_group, server_name, params,)
+    async_server_create.result()
+
+    set_progress(
+        'Creating database "%s" on server "%s"...' % (database_name, server_name)
+    )
+    async_db_create = client.databases.create_or_update(
+        resource_group, server_name, database_name, {"location": location}
+    )
+    database = async_db_create.result()  # Wait for completion and return created object
+    assert database.name == database_name
+
+    db = client.databases.get(resource_group, server_name, database_name)
+    assert db.name == database_name
+
+    set_progress('Database "%s" has been created.' % database_name)
