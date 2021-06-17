fbda39ecf76d55fed11980707aa15fe082d61b77
mbaker <mbaker@cloudbolt.io>
2021-06-17 09:54:15 -0700

    [DEV-19144] Remove from ui_extensions init, put django version check in the views import

--- ui_extensions/__init__.py
+++
@@ -1,7 +0,0 @@
-import django
-if int(django.get_version()[0]) < 3:
-    from django.contrib.staticfiles.templatetags.staticfiles import static
-else:
-    from django.templatetags.static import static
-
-__all__ = ["static"]
