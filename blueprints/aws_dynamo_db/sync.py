caeaf217d0c67ccc9e243f627871da61db738da7
pushkarg-kumolus <pushkarg@kumolus.com>
2021-11-17 17:03:40 +0530

    [DEV-20672] - AWS Dynamo DB - Blueprint is broken - Fix

--- blueprints/aws_dynamo_db/sync.py
+++ blueprints/aws_dynamo_db/sync.py
@@ -7,6 +7,7 @@ from accounts.models import Group
 import boto3
 from common.methods import set_progress
 
+
 RESOURCE_IDENTIFIER = 'table_name'
 
 
@@ -19,13 +20,15 @@ def discover_resources(**kwargs):
     dynamodb_resource_type = ResourceType.objects.filter(name__iexact='Database').first()
 
     for rh in AWSHandler.objects.all():
-        environments = Environment.objects.filter(resource_handler=rh)
-
-        for env in environments:
-            if env.aws_region:
+        
+        # Retrieving unique list of active/imported regions for 'rh' resource handler
+        imported_region_list = list(rh.current_regions())
+        
+        for region in imported_region_list:
+            if region:
                 dynamodb = boto3.client(
                     'dynamodb',
-                    region_name=env.aws_region,
+                    region_name=region,
                     aws_access_key_id=rh.serviceaccount,
                     aws_secret_access_key=rh.servicepasswd,
                 )
@@ -36,7 +39,7 @@ def discover_resources(**kwargs):
                         data = {
                             'name': table,
                             'aws_rh_id': rh.id,
-                            'aws_region': env.aws_region,
+                            'aws_region': region,
                             'table_name': table,
                         }
                         # Discover backups for this table
@@ -44,39 +47,10 @@ def discover_resources(**kwargs):
                                 TableName=table)
 
                         if data not in discovered_tables:
-                            resource, _ = Resource.objects.get_or_create(
-                                name=table,
-                                blueprint=dynamodb_blueprint,
-                                defaults={
-                                    'group': group,
-                                    'lifecycle': 'Active',
-                                    'resource_type': dynamodb_resource_type})
-
-                            resource.aws_rh_id = rh.id
-                            resource.aws_region = env.aws_region
-                            resource.table_name = table
-                            resource.save()
-
-                            if response:
-                                backups = response['BackupSummaries']
-
-                                for backup in backups:
-                                    res, _ = Resource.objects.get_or_create(
-                                        name=backup['BackupName'],
-                                        blueprint=blueprint,
-                                        defaults={
-                                            'group': group,
-                                            'parent_resource': resource,
-                                            'lifecycle': 'Active',
-                                            'resource_type': resource_type})
-
-                                    res.backup_arn = backup['BackupArn']
-                                    res.aws_rh_id = rh.id
-                                    res.aws_region = env.aws_region
-                                    res.save()
-
                             discovered_tables.append(data)
+                            
                 except Exception as e:
                     set_progress(e)
                     continue
+
     return discovered_tables
