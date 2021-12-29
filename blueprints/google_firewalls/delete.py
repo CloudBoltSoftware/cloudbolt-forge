63ba9cd06fd80ea964a21657005e473fa446cd0f
yogeshmhaskule93 <ymhaskule@cloudbolt.io>
2021-12-29 18:00:42 +0530

    Fixed code for google firewall blueprint as resource handler change frm GCE to GCP

--- blueprints/google_firewalls/delete.py
+++ blueprints/google_firewalls/delete.py
@@ -1,40 +1,35 @@
 """
 Teardown service for google firewall rules.
 """
+import json
 
 from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
 from oauth2client.service_account import ServiceAccountCredentials
-from resourcehandlers.gce.models import GCEHandler
+from resourcehandlers.gcp.models import GCPHandler
 from common.methods import set_progress
-import time
+
 
 
 def run(job, logger=None, **kwargs):
-    resource = kwargs.pop('resources').first()
-
+    
+    resource = kwargs.get("resource")
     google_firewall_name = resource.attributes.get(field__name='google_firewall_name').value
+    project_id = resource.attributes.get(field__name='google_cloud_project').value
     rh_id = resource.attributes.get(field__name='google_rh_id').value
-    rh = GCEHandler.objects.get(id=rh_id)
+    rh = GCPHandler.objects.get(id=rh_id)
 
-    project = rh.project
-
-    set_progress('Connecting to Google Cloud')
-
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict({
-        'client_email': rh.serviceaccount,
-        'private_key': rh.servicepasswd,
-        'type': 'service_account',
-        'project_id': project,
-        'client_id': None,
-        'private_key_id': None,
-    })
-
-    client = build('compute', 'v1', credentials=credentials)
+    set_progress('Connecting to Google Cloud...')
+    
+    credentials_dict = json.loads(rh.gcp_api_credentials)
+    credentials = Credentials(**credentials_dict)
+    client = build("compute", "v1", credentials=credentials, cache_discovery=False)
+    
     set_progress("Connection to google cloud established")
 
     set_progress('Deleting firewall rule %s...' % google_firewall_name)
 
     #delete the firewall
-    client.firewalls().delete(project=project, firewall=google_firewall_name).execute()
+    client.firewalls().delete(project=project_id, firewall=google_firewall_name).execute()
 
     return "SUCCESS", "The firewall has been deleted", ""
--- blueprints/google_firewalls/sync.py
+++ blueprints/google_firewalls/sync.py
@@ -2,10 +2,12 @@
 Discover google cloud firewall rules
 """
 from common.methods import set_progress
-from resourcehandlers.gce.models import GCEHandler
+from infrastructure.models import Environment
+from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
-
+from resourcehandlers.gcp.models import GCPHandler
+import json
 
 RESOURCE_IDENTIFIER = 'google_firewall_name'
 
@@ -13,41 +15,41 @@ RESOURCE_IDENTIFIER = 'google_firewall_name'
 def discover_resources(**kwargs):
     discovered_firewall_rules = []
 
-    projects = {rh.project for rh in GCEHandler.objects.all()}
+    gcp_envs = Environment.objects.filter(
+        resource_handler__resource_technology__name="Google Cloud Platform")
+    projects = {env.gcp_project for env in gcp_envs}
 
-    for handler in GCEHandler.objects.all():
-        set_progress('Connecting to GCE for handler: {}'.format(handler))
-
-        project = handler.project
-
-        if project not in projects:
-            continue
-        credentials = ServiceAccountCredentials.from_json_keyfile_dict({
-            'client_email': handler.serviceaccount,
-            'private_key': handler.servicepasswd,
-            'type': 'service_account',
-            'project_id': project,
-            'client_id': None,
-            'private_key_id': None,
-        })
-
-        client = build('compute', 'v1', credentials=credentials)
-        set_progress("Connection to google cloud established")
-
-        rules = client.firewalls().list(project=project).execute()
-
-        if rules["items"]:
-            for rule in rules['items']:
-                discovered_firewall_rules.append(
-                    {
-                        'google_rh_id': handler.id,
-                        'name': rule['name'],
-                        'google_firewall_name': rule['name'],
-                        "google_firewall_priority": rule['priority'],
-                        "google_firewall_direction": rule['direction'],
-                        "google_cloud_project": project
-                    }
-                )
-        projects.discard(project)
-
+    for handler in GCPHandler.objects.all():
+        set_progress('Connecting to GCP handler: {}'.format(handler))
+        
+        #only get firewall of projects in the set
+        for env in Environment.objects.filter(resource_handler_id=handler.id):
+            project_id = env.gcp_project
+            if project_id not in projects:
+                continue
+            
+            credentials_dict = json.loads(handler.gcp_api_credentials)
+            credentials = Credentials(**credentials_dict)
+            client = build("compute", "v1", credentials=credentials, cache_discovery=False)
+            
+            set_progress("Connection to google cloud established")
+            
+            gcp_project = handler.gcp_projects.get(id = project_id).gcp_id
+            
+            rules = client.firewalls().list(project=gcp_project).execute()
+    
+            if rules["items"]:
+                for rule in rules['items']:
+                    discovered_firewall_rules.append(
+                        {
+                            'google_rh_id': handler.id,
+                            'name': rule['name'],
+                            'google_firewall_name': rule['name'],
+                            "google_firewall_priority": rule['priority'],
+                            "google_firewall_direction": rule['direction'],
+                            "google_cloud_project": gcp_project
+                        }
+                    )
+        projects.discard(gcp_project)
+    
     return discovered_firewall_rules
