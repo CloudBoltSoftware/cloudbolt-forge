3a01df1f32707cdb77f76e0249c9c4d7adb0c44e
yogeshmhaskule93 <ymhaskule@cloudbolt.io>
2022-01-28 13:06:17 +0530

    Fixed minor chnages and updated log messages

--- actions/cloudbolt_plugins/maas/configure_raid/Provision_RAID_Physical_Server.py
+++ actions/cloudbolt_plugins/maas/configure_raid/Provision_RAID_Physical_Server.py
@@ -21,16 +21,15 @@ def run(job, *args, **kwargs):
     #server_id is the ID of the Cloudbolt Server being provisioned
     server_id = kwargs.get('server_id', None)
     cb_server = Server.objects.get(pk=server_id)
-    
-    is_physical = kwargs.get('physical', False)
-    
-    if not is_physical:
+
+    # maas_machine_type = True when create virtual machine else False
+    if  cb_server.maas_machine_type:
         set_progress("Provisioning a RAID is not valid for the virtual machine on a MAAS LXD host")
         return "SUCCESS", "The MaaS during-provisioning plugin has completed.", ""
     
     if machine:
         maas_id = machine['system_id']
-        set_progress("The MaaS plugine will fix up your MAAS machine {}, Cloudbolt Server id = {}; please stand by.".format(maas_id, server_id))
+        set_progress("Configuring RAID on MAAS machine {}".format(maas_id))
     else:
         return "FAILURE", "", "Missing required MAAS machine info."
 
@@ -38,29 +37,27 @@ def run(job, *args, **kwargs):
     if wrapper:
         vm = wrapper.get_vm('unused', maas_id)
         machine = wrapper.driver.get_machine(maas_id)
-        set_progress("The MaaS wrapper is valid.  hostname={}".format(vm['hostname']))
+    else:
+        return "FAILURE", "", "Invalid (None) wrapper passed to plugin"
 
-    ipv4_gateway = machine['default_gateways']['ipv4']['gateway_ip']
-    mac_addr = machine['boot_interface']['mac_address']
-    set_progress("MAC Address: {} -- IPv4 Gateway: {}".format(mac_addr, ipv4_gateway))
-  
     fstype = "ext4"
-    set_progress("Provisioning a physical MAAS machine.")
     # remove any existing RAID configurations
     raids = wrapper.maas_api_request("GET", "nodes/{}/raids/".format(maas_id))
+    
     if raids:
+        # We need to move to machine in ready state to delete the existig raid, you can't delete raid if machine in allocated state
         rel = wrapper.maas_api_request("POST", "machines/{}/?op=release".format(maas_id), data = {"comment" : "making in ready state" })
-
-    for raid in raids:
-        set_progress("Deleting RAID fields: {}".format(raid['name']))
-        wrapper.maas_api_request("DELETE", "nodes/{}/raid/{}/".format(maas_id, raid['id']))
-
+        for raid in raids:
+            set_progress("Deleting RAID volume: {}".format(raid['name']))
+            wrapper.maas_api_request("DELETE", "nodes/{}/raid/{}/".format(maas_id, raid['id']))
+            
     # get unused physical disks
     all_disks = wrapper.maas_api_request("GET", "nodes/{}/blockdevices/".format(maas_id))
     disks = [str(disk['id']) for disk in all_disks if disk['used_for'] == 'Unused' and disk['type'] == 'physical']
     set_progress("MaaS machine has {} associated disks, of which {} are unused physical disks.".format(len(all_disks), len(disks)))
+    
     if  len(disks)>=2:
-        set_progress("Creating a RAID array for the machine.")
+        set_progress("Creating a RAID-1 array md0 for the machine using all unused disks: {}".format(','.join([disk['name'] for disk in all_disks if disk['used_for'] == 'Unused' and disk['type'] == 'physical'])))
         data={
             "name": "md0",
             "uuid": uuid.uuid4(),
@@ -70,25 +67,24 @@ def run(job, *args, **kwargs):
             "spare_devices": [],
             "spare_partitions": [],
             }
-    
-        rel = wrapper.maas_api_request("POST", "machines/{}/?op=release".format(maas_id), data = {"comment" : "making in ready state" })
+        
         resp = wrapper.maas_api_request("POST", "nodes/{}/raids/".format(maas_id), data= data)
         block_device_id = resp["virtual_device"]["id"]
         
-        set_progress("Filesystem Format: block_device_id={}, fstype={}".format(block_device_id, fstype))
+        set_progress("Formatting filesystem: block_device_id={}, fstype={}".format(block_device_id, fstype))
         formatdata ={
             'fstype': fstype, 
             'uuid': uuid.uuid4()
         }
         formatres = wrapper.maas_api_request("POST", 'nodes/{}/blockdevices/{}/?op=format'.format(maas_id, block_device_id), data = formatdata)
         
-        set_progress("Filesystem mount: block_device_id={}, mount_point={}".format(block_device_id, mount_point))
+        set_progress("Configuring mount point: block_device_id={}, mount_point={}".format(block_device_id, mount_point))
         mountdata={
             "mount_point": mount_point,
             "mount_options": "",
         }
         mountres = wrapper.maas_api_request("POST", 'nodes/{}/blockdevices/{}/?op=mount'.format(maas_id, block_device_id), data = mountdata)
 
-        return "SUCCESS", "The MaaS during-provisioning plugin has completed.", ""
+        return "SUCCESS", "Finished configuring RAID for MaaS machine", ""
     else:
         return "FAILURE", "", "Failed to create RAID during-provisioning because only {} disk, It should be have more than 2 disks".format(disks)
