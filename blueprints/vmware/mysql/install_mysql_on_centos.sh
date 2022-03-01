7978a52eab157b6b5d33dad80c5bed284e32f1e7
Ishu Kumar <ikumar@cloudbolt.io>
2022-03-01 12:59:19 +0530

    removed comments and extra code

--- blueprints/vmware/mysql/install_mysql_on_centos.sh
+++ blueprints/vmware/mysql/install_mysql_on_centos.sh
@@ -4,29 +4,13 @@
 
 mysqlRootPass="{{ server.database_root_password }}"
 
-echo ' -> Removing previous mysql server installation'
 service mysqld stop && yum remove -y mysql-server && rm -rf /var/lib/mysql && rm -rf /var/log/mysqld.log && rm -rf /etc/my.cnf
 
-echo ' -> Installing mysql server rpm package from repo'
-yum install -y https://repo.mysql.com/mysql80-community-release-el7.rpm
-echo ' -> Importing GPG keys online'
-rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
 echo ' -> Installing mysql server (community edition)'
+yum install -y https://repo.mysql.com/mysql80-community-release-el7.rpm
+rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
 yum install -y mysql-server
 
-# commands to create a swap parition for a smaller sized instance
-# echo ' -> Adding swap partition for fixinf memory issues'
-# dd if=/dev/zero of=/swapfile bs=1M count=1024
-# mkswap /swapfile
-# chmod 0600 /swapfile
-# swapon /swapfile
-# echo '/swapfile swap swap defaults 0 0' >> /etc/fstab
-# # swapon -s
-# # free -k
-# # swapon -a
-# # swapoff -a
-
-echo ' -> adding configuration to my.cnf file to access mysql without default password'
 echo '
 [mysqld]
 
@@ -38,21 +22,15 @@ bind-address=127.0.0.1
 
 socket=/var/lib/mysql/mysql.sock' >> /etc/my.cnf
 
-echo ' -> Starting mysql server (first run)'
 /sbin/chkconfig --levels 235 mysqld on
 service mysqld start
 
-echo ' -> Extracting temp root password'
 tempRootDBPass="`grep 'temporary.*root@localhost' /var/log/mysqld.log | tail -n 1 | sed 's/.*root@localhost: //'`"
 
-echo ' -> Stopping mysql service'
 service mysqld stop
-echo ' -> Deleting log file'
 rm -rf /var/lib/mysql/*logfile*
-echo ' -> Starting mysql server service'
 service mysqld start
 
-echo ' -> Enabling root login to mysql and setting up new mysql server root password'
 mysql -u root <<-EOSQL
     UPDATE mysql.user SET authentication_string=null WHERE User='root';
     FLUSH PRIVILEGES;
@@ -60,13 +38,10 @@ EOSQL
 
 mysql -u root --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '${mysqlRootPass}';"
 
-echo ' -> Fixing mysql configuration to run mysql securely'
 sed -i 's/skip-grant-tables/#skip-grant-tables/g' /etc/my.cnf
 sed -i 's/bind-address=127.0.0.1/#bind-address=127.0.0.1/g' /etc/my.cnf
 
-echo ' -> Starting mysql server'
 service mysqld restart
-echo ' -> Setting up mysql server service'
 mysql -u root --password="$mysqlRootPass" <<-EOSQL
     DELETE FROM mysql.user WHERE User='';
     DROP DATABASE IF EXISTS test; 
