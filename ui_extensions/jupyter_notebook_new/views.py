aaf6726bb466d22ddcd6564d5238eed1299561c8
Ella Blackledge <eblackledge@cloudbolt.io>
2023-09-06 16:50:03 -0500

    [ENG-25562] Reverted accidental changes.

--- ui_extensions/jupyter_notebook_new/views.py
+++ ui_extensions/jupyter_notebook_new/views.py
@@ -5,13 +5,13 @@ from utilities.views import (CBAdminRequiredMixin, GlobalPreferenceRequiredMixin
 from django.views.generic import TemplateView
 from portals.models import PortalConfig
 
-class JupyterNotebook(TabExtensionDelegate):
+class JupyterNotebookNew(TabExtensionDelegate):
     def should_display(self):
         return True
 
 @admin_extension(title='Jupyter Notebooks', description='Jupyter...')
-def jupyter_notebook_view(request):
-    current_domain = PortalConfig.get_current_domain(request) + ":" + settings.NOTEBOOK_PORT
+def jupyter_notebook_new_view(request):
+    current_domain = PortalConfig.get_current_domain(request)
     notebook_base_uri = settings.NOTEBOOK_BASE_URI if hasattr(settings, 'NOTEBOOK_BASE_URI') else current_domain
     notebook_token = settings.NOTEBOOK_TOKEN
     notebook_uri = '{}/shell/?token={}'.format(notebook_base_uri, notebook_token)
@@ -19,4 +19,4 @@ def jupyter_notebook_view(request):
         'notebook_uri': notebook_uri,
         'pagetitle': 'Django Notebook'
     }
-    return render(request, 'jupyter_notebook/templates/django-notebook.html', context)
+    return render(request, 'jupyter_notebook_new/templates/django-notebook_new.html', context)
