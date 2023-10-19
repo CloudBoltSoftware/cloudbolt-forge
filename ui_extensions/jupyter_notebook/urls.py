d8f6dcf28d583a7f60684bb99e0b81fc872ce88e
Ella Blackledge <eblackledge@cloudbolt.io>
2023-10-18 20:16:26 -0500

    Deleted old Jupyter XUI.

--- ui_extensions/jupyter_notebook/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from xui.jupyter_notebook import views
-
-xui_urlpatterns = [
-    url(r'^notebook', views.jupyter_notebook_view, name='django_notebook'),
-    url(r'^restart_jupyterd', views.restart_jupyterd, name='restart_jupyterd')
-]
