e6dd3a5bdf7a9adb5f612492c1290b6c7cb4cf66
Ella Blackledge <ellablac@gmail.com>
2024-02-13 20:09:57 -0600

    Update cb_plugin_1706898870553853.py
    Troubleshooting Blueprints from Remote INC-100

--- blueprints/sample_blueprint_from_remote_source/action_added_in_sample_blueprint_from_remotesource/cb_plugin_1706898870553853.py
+++ blueprints/sample_blueprint_from_remote_source/action_added_in_sample_blueprint_from_remotesource/cb_plugin_1706898870553853.py
@@ -1,7 +1,7 @@
 """
-UPDATED: This is a working sample CloudBolt plug-in for you to start with. The run method is required,
+UPDATED This is a working sample CloudBolt plug-in for you to start with. The run method is required,
 but you can change all the code within it. See the "CloudBolt Plug-ins" section of the docs for
-more info and the CloudBolt forge for more examples:
+more info and the CloudBolt forge for more examples: 
 https://github.com/CloudBoltSoftware/cloudbolt-forge/tree/master/actions/cloudbolt_plugins
 """
 from common.methods import set_progress
