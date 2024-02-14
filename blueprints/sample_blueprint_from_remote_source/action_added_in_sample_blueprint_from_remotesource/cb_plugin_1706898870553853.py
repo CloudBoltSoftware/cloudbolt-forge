84cd3e30afccfa02e6b012bbd07e6a97d958087b
Ella Blackledge <ellablac@gmail.com>
2024-02-13 18:30:15 -0600

    Update cb_plugin_1706898870553853.py
    Attempting to recreate scenario described in INC-100: changed the plugin source code comment

--- blueprints/sample_blueprint_from_remote_source/action_added_in_sample_blueprint_from_remotesource/cb_plugin_1706898870553853.py
+++ blueprints/sample_blueprint_from_remote_source/action_added_in_sample_blueprint_from_remotesource/cb_plugin_1706898870553853.py
@@ -1,5 +1,5 @@
 """
-This is a working sample CloudBolt plug-in for you to start with. The run method is required,
+UPDATED: This is a working sample CloudBolt plug-in for you to start with. The run method is required,
 but you can change all the code within it. See the "CloudBolt Plug-ins" section of the docs for
 more info and the CloudBolt forge for more examples:
 https://github.com/CloudBoltSoftware/cloudbolt-forge/tree/master/actions/cloudbolt_plugins
